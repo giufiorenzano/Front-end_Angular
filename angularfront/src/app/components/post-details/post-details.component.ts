d013307db8c12655293ed0178b533367455354da
Giulia Fiorenzano <giufiorenzano@gmail.com>
2020-10-17 15:33:33 -0300

    Finalizando a aplicação
    Finalizando a aplicação criando a parte de "post details component" com as opções de editar, excluir e publicar.

--- angularfront/src/app/components/post-details/post-details.component.ts
+++ angularfront/src/app/components/post-details/post-details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PostService } from 'src/app/services/post.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-post-details',
@@ -6,10 +8,71 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post-details.component.css']
 })
 export class PostDetailsComponent implements OnInit {
+  currentPost = null;
+  message = '';
 
-  constructor() { }
+  constructor(
+    private postService: PostService,
+    private route: ActivatedRoute,
+    private router: Router  
+  ) {  }
 
   ngOnInit(): void {
+    this.message = "";
+    this.getPost(this.route.snapshot.paramMap.get('id'));
   }
 
+  getPost(id): void {
+    this.postService.get(id)
+    .subscribe(
+      data => {
+        this.currentPost = data;
+        console.log(data);
+      },
+      error => {
+        console.log(error);
+      });
+    }
+
+    updatePost(): void {
+      this.postService.update(this.currentPost.id, this.currentPost)
+      .subscribe(
+        response => {
+          console.log(response);
+          this.message = 'Esta postagem foi atualizada com sucesso.';
+        },
+        error => {
+          console.log(error);
+        });
+    }
+
+    deletePost(): void {
+      this.postService.delete(this.currentPost.id)
+      .subscribe(
+        response => {
+          console.log(response);
+          this.router.navigate(['/posts'])
+        },
+        error => {
+          console.log(error);
+        });
+    }
+
+    updatePublished(status): void {
+      const data = {
+        title: this.currentPost.title,
+        description: this.currentPost.description,
+        published: status
+      };
+
+      this.postService.update(this.currentPost.id, data)
+      .subscribe(
+        response => {
+          this.currentPost.published = status;
+          console.log(response);
+        },
+        error => {
+          console.log(error);
+        });
+    }
 }
